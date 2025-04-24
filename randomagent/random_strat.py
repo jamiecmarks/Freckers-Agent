a56ef5a68eee1f47bacbccbb59966c2e74cce726
jamiepro <jamieacmarks@gmail.com>
2025-04-24 11:42:10 +1000

    Add weighted random agent

--- randomagent/random_strat.py
+++ randomagent/random_strat.py
@@ -1,10 +1,12 @@
 import numpy as np
 from referee.game.actions import GrowAction, MoveAction
 from agent.strategy import Strategy
+import random
 
 
 class RandomStrat(Strategy):
     def __init__(self, state):
+        self.weighted = True
         super().__init__(state)
 
     def find_child(self, action):
@@ -14,7 +16,19 @@ class RandomStrat(Strategy):
 
     def best_action(self):
         possible_moves = self.state.get_all_moves()
-        idx = np.random.randint(len(possible_moves))
+        if self.weighted:
+            vert_dists = []
+            for mv, res in possible_moves:
+                if isinstance(mv, GrowAction):
+                    vert_dists.append(1)  # just count MoveAction as vert_dist of 1
+                else:
+                    vert_dists.append(abs(mv.coord.r - res.r))
+            max_dist = max(vert_dists)
+            idx = random.choice(
+                [i for i, dist in enumerate(vert_dists) if max_dist == dist]
+            )
+        else:
+            idx = np.random.randint(len(possible_moves))
         action = possible_moves[idx][0]
         # res = None
         # if not isinstance(action, GrowAction):
